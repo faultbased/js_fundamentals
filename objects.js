3f2185bed4f8005e645cb49182a809b04528a435
faultbased <s_cammorto@outlook.com>
2022-04-13 10:33:56 -0400

    objects and return multiple values

---
+++ objects.js
@@ -0,0 +1,27 @@
+const person1 = {
+	firstName: "Joffery",
+	lastName: "Barfly",
+	email: "joff@barstool.com",
+};
+
+const person2 = {
+	// key-value pairing; keys on left, value on right
+	firstName: "Dean",
+	lastName: "Winchester",
+	email: "win@devilhorn.com",
+};
+
+//since you can't return multi values, you can wrap them in another object to be returned
+const getEmailAndlastName = () => {
+	return {
+		email: person2.email,
+		lastName: person2.lastName,
+	};
+};
+
+//Not Available
+read = () => {
+	console.info(getEmailAndlastName());
+};
+
+read();
