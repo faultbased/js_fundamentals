4456cd52a1dbcd94e1800709ddb0973c9488455c
faultbased <s_cammorto@outlook.com>
2022-04-13 12:46:19 -0400

    updated interm_callback.js

--- interm_callback.js
+++ interm_callback.js
@@ -1,3 +1,13 @@
 import { stocks } from "./stockArray.js";
 
-console.info(stocks.Fruits[1]);
+let order = (call_production) => {
+	console.info("Order placed. Please call production");
+
+	call_production();
+};
+
+let production = () => {
+	console.info("Production has started");
+};
+
+order(production);
