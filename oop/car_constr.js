c2bcfe017be9c0caa538690f8f1ad0676dbd4e46
faultbased <s_cammorto@outlook.com>
2022-04-30 22:23:01 -0400

    simple constructor/class

---
+++ oop/car_constr.js
@@ -0,0 +1,11 @@
+class car {
+	constructor(color, brand, model) {
+		this.color = color;
+		this.brand = brand;
+		this.model = model;
+	}
+}
+
+const carK = new car("Earth Green", "Honda", "Civic SE");
+
+console.info(carK);
